6ebc9438938df6a9dd7987f516d089fbbc542057
koalasym <70763281+koalasym@users.noreply.github.com>
2022-08-11 20:50:12 +0800

    Add files via upload

---
+++ js/holla.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {    
+     //header
+     var new_scroll_position = 0;
+     var last_scroll_position;
+     var header = document.getElementById("header");
+ 
+     window.addEventListener('scroll', function(e) {
+       last_scroll_position = window.scrollY;
+ 
+       if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+         header.classList.remove("slideDown");
+         header.classList.add("slideUp");
+ 
+       } else if (new_scroll_position > last_scroll_position) {
+         header.classList.remove("slideUp");
+         header.classList.add("slideDown");
+       }
+ 
+       new_scroll_position = last_scroll_position;
+     });	
+  
+  //nav
+     $("#mnavh").click(function(){
+     $("#starlist").toggle();
+     $("#mnavh").toggleClass("open");
+     });
+    
+ });
+ 
+ 
+ 
+ 
+
